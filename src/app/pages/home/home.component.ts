fce7411887ef9f67fe336ee9205ba7cd20463d6e
diopmorfall <morfall.papydiop99@gmail.com>
2025-03-25 20:56:59 +0100

    Initiation of products-header

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+import { ProductsHeaderComponent } from "./components/products-header/products-header.component";
+
+@Component({
+    selector: 'app-home',
+    standalone: true,
+    imports: [MatSidenavModule, ProductsHeaderComponent],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
